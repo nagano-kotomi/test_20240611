ccf577256c18e9722183d769b3ad180054c82b40
永野琴実 <k2nagano@oec.co.jp>
2024-06-24 09:07:09 +0900

    add/Vueの学習

---
+++ vue_learning/src/components/Form.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+      <div class="item">
+        <input type="text" name="title" placeholder="タイトル" v-model="inputTitle">
+      </div>
+      <div class="item">
+        <input type="text" name="period" placeholder="詳細" v-model="inputPeriod">
+      </div>
+      <div class="item">
+        完了期限
+        <input type="date" name="detail" placeholder="完了期間" v-model="inputDetail"></input>
+      </div>
+      <div class="item">
+        <button v-on:click="createTask">作成</button>
+      </div>
+    <div>
+      <table border="1">
+        <tr>{{ DisplayTitle }}</tr>
+        <tr>{{ DisplayPeriod }}</tr>
+        <tr>{{ DisplayDetail }}</tr>
+    </table>
+    </div>
+    </div>
+  </template>
+  <!-- textare -->
+
+<script>
+export default {
+  data() {
+    return {
+      inputTitle: "",
+      inputPeriod: "",
+      inputDetail: "",
+      DisplayTitle: "",
+      DisplayPeriod: "",
+      DisplayDetail: ""
+    }
+  }
+  ,methods: {
+      createTask() {
+          // タスクを作成する処理
+        this.DisplayTitle = this.inputTitle
+        this.DisplayPeriod = this.inputPeriod
+        this.DisplayDetail = this.inputDetail
+      }
+  }
+}
+</script>
---
+++ vue_learning/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<!-- <script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template> -->
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink to="/learn">Go to Learn</RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+/* 任意のスタイルを追加 */
+</style>
